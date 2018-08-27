57a92b2a2012761c232d4eadcbda992088d27aa4
Felix Godfree <felixfgodfree@gmail.com>
2018-08-27 13:07:14 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -23,6 +23,8 @@ function doWhileLoop(array) {
   }
   do {
     array.pop();
-  } while (array.length > 0 && incrementVariable());
+  } while (
+    array.length > 0 && incrementVariable()
+  );
   return array;
 }
