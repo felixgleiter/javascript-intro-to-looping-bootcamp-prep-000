cf6cd478ce82d613ede19e264c0ad1dec8a045ec
Felix Godfree <felixfgodfree@gmail.com>
2018-08-27 12:53:58 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,8 +1,8 @@
 function forLoop(array) {
-  for (var i = 0; i < 25; i++) {
+  for (let i = 0; i < 25; i++) {
     if (i == 1) {
       array.push("I am 1 strange loop.")
-    } else {
+    } else  {
       array.push(`"I am ${i} strange loops."`);
     }
   }
